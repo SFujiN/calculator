5b156040f5fb3134a8c4b8424d5b731a3606f121
SFujiN <d0n4lddanghuynh@gmail.com>
2023-03-22 14:14:43 -0500

    Add basic functions

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function add(l, r) {
+  return l + r;
+}
+
+function subtract(l, r) {
+  return l - r;
+}
+
+function multiply(l, r) {
+  return l * r;
+}
+
+function divide(l, r) {
+  if (!r) {
+    return NaN;
+  }
+  return l / r;
+}
